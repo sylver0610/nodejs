c45281fc74fa6d935922f8c75b4fafed181ff6cc
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2022-09-22 23:13:09 +0300

    create partials, change filename handlebars -> hbs

--- src/index.js
+++ src/index.js
@@ -9,13 +9,19 @@ const port = 3000
 app.use(morgan('combined'))
 
 //template engine
-app.engine('handlebars',handlebars.engine());
+app.engine('hbs',handlebars.engine({
+    extname: 'hbs'
+}));
 
-app.set('view engine','handlebars');
+app.set('view engine','hbs');
 app.set('views',path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {    
   res.render('home');
+})
+
+app.get('/news', (req, res) => {    
+  res.render('news');
 })
 
 app.listen(port, () => {
